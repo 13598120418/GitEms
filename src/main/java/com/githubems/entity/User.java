b81843ea3598b6ac7755179e047de15f0648a57a
kdh <1007806829@qq.com>
2019-12-07 12:14:05 +0800

    第一次修改

---
+++ src/main/java/com/githubems/entity/User.java
@@ -0,0 +1,18 @@
+package com.githubems.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable{
+    private String username;
+    private String password;
+}
